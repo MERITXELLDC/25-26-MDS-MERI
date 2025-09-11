c53a7752f3e63bf84047930b51dd64eb0e0704cc
meritxell <meritxell.durany@itb.cat>
2025-09-11 17:45:07 +0200

    commit edats

---
+++ TA02/comprovaedat.py
@@ -0,0 +1,9 @@
+# Programa que demana l'edat i diu si ets major d'edat.
+
+edat=int(input("Quina edat tens?"))
+if edat>=18:
+    print("Ets major d'edat")
+    print("Programa fnalitzat")
+else:
+    print("Ets menor d'edat")
+    print("Programa Finalitzat")
